658f4489503aa2f7b19207742bb57a03d38cc99a
Sihap <baharuddinsihap@gmail.com>
2024-08-30 23:28:30 +0700

    feat: hide log activity on role sysadmin

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -86,17 +86,18 @@
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Customers</span>
                 </a>
             </li>
-            <li class="mt-0.5 w-full">
-                <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors {{ request()->routeIs('dashboard.log_activity.index') ? 'bg-blue-500/13 text-slate-700 font-semibold' : '' }}"
-                    href="{{ route('dashboard.log_activity.index') }}">
-                    <div
-                        class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                        <i class="relative top-0 text-sm leading-normal text-orange-600 ni ni-briefcase-24"></i>
-                    </div>
-                    <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Catatan Aktivitas</span>
-                </a>
-            </li>
-
+            @if (auth()->user()->role !== 'sysadmin')
+                <li class="mt-0.5 w-full">
+                    <a class=" dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors {{ request()->routeIs('dashboard.log_activity.index') ? 'bg-blue-500/13 text-slate-700 font-semibold' : '' }}"
+                        href="{{ route('dashboard.log_activity.index') }}">
+                        <div
+                            class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-orange-600 ni ni-briefcase-24"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Catatan Aktivitas</span>
+                    </a>
+                </li>
+            @endif
 
         </ul>
     </div>
