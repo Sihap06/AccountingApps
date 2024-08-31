7a6be6c53f1c54e77c077df9233c7f7a11c61a73
Sihap <baharuddinsihap@gmail.com>
2024-08-31 22:28:31 +0700

    fix z-index on sidebar

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <!-- sidenav  -->
 <aside
-    class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand xl:ml-6 z-90 rounded-2xl xl:left-0 md:translate-x-0"
+    class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand xl:ml-6 z-100 rounded-2xl xl:left-0 md:translate-x-0"
     aria-expanded="false">
 
     <div class="h-auto">
