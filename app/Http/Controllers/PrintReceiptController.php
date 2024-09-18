e45c4c9e65b7ba3ab4e7301247c32a04daf1b881
Sihap <baharuddinsihap@gmail.com>
2024-09-18 21:57:23 +0700

    fix

--- app/Http/Controllers/PrintReceiptController.php
+++ app/Http/Controllers/PrintReceiptController.php
@@ -14,13 +14,13 @@ class PrintReceiptController extends Controller
     {
         try {
             // For Windows printer setup
-            $connector = new WindowsPrintConnector("EPSON_LX_310"); // Name of the printer
+            // $connector = new WindowsPrintConnector("EPSON_LX_310"); // Name of the printer
 
             // For network printer setup
             // $connector = new NetworkPrintConnector("192.168.0.123", 9100);
 
             // For USB or parallel port (Linux)
-            // $connector = new FilePrintConnector("/dev/usb/lp0");
+            $connector = new FilePrintConnector("/dev/usb/lp0");
 
             $printer = new Printer($connector);
 
