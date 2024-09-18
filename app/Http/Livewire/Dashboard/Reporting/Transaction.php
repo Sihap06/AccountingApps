2d406ec9c6e20ccc11edf3037508f866d063a8f7
Sihap <baharuddinsihap@gmail.com>
2024-09-18 22:25:55 +0700

    test exec pdftotext

--- app/Http/Livewire/Dashboard/Reporting/Transaction.php
+++ app/Http/Livewire/Dashboard/Reporting/Transaction.php
@@ -353,6 +353,8 @@ class Transaction extends Component
         $pdfPath = 'public/nota/receipt.pdf';
         Storage::put($pdfPath, $pdf->output());
 
+        exec('pdftotext public/nota/receipt.pdf public/nota/receipt.txt');
+
         // Dapatkan URL publik dari file PDF yang disimpan
         $pdfUrl = Storage::url($pdfPath);
 
